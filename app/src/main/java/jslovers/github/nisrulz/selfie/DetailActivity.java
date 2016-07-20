4c0cf0be7bb28d371bbf73e71d0ef08594a147cf
Nishant Srivastava <nisrulz@gmail.com>
2016-07-20 13:57:33 +0530

    added details activity

---
+++ app/src/main/java/jslovers/github/nisrulz/selfie/DetailActivity.java
@@ -0,0 +1,12 @@
+package jslovers.github.nisrulz.selfie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_detail);
+  }
+}
