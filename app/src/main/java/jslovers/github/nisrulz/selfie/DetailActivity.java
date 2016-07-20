4ba4db97922898354c2b387a54205a9ff62148b1
Nishant Srivastava <nisrulz@gmail.com>
2016-07-20 14:05:33 +0530

    implemented the details activity

--- app/src/main/java/jslovers/github/nisrulz/selfie/DetailActivity.java
+++ app/src/main/java/jslovers/github/nisrulz/selfie/DetailActivity.java
@@ -1,12 +1,25 @@
 package jslovers.github.nisrulz.selfie;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_detail);
+
+    // get data from incoming intent
+    String imagepath = getIntent().getExtras().getString("path");
+    String name = getIntent().getExtras().getString("name");
+
+    // Get reference to imageview
+    ImageView imgvw_details = (ImageView) findViewById(R.id.imgvw_selfie_detail);
+
+    // Set image in imageview
+    ImageHelper.setImageFromFilePath(this, imagepath, imgvw_details);
+    // Set title of the activity
+    getSupportActionBar().setTitle(name);
   }
 }
