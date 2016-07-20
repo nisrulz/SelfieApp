fea4abc0cc4657039e2dfb12fd08f71299cf27ce
Nishant Srivastava <nisrulz@gmail.com>
2016-07-20 14:19:28 +0530

    implemented funtionality to take pictures

--- app/src/main/java/jslovers/github/nisrulz/selfie/MainActivity.java
+++ app/src/main/java/jslovers/github/nisrulz/selfie/MainActivity.java
@@ -1,9 +1,11 @@
 package jslovers.github.nisrulz.selfie;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,13 +15,22 @@ import android.view.MenuItem;
 import android.view.View;
 import github.nisrulz.recyclerviewhelper.RVHItemClickListener;
 import github.nisrulz.recyclerviewhelper.RVHItemDividerDecoration;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+  private static final int REQUEST_IMAGE_CAPTURE = 100;
 
   RecyclerView myrecyclerview;
   ArrayList<Selfie> selfieList;
   SelfieListAdapter adapter;
+
+  String currentSelfiePath;
+  String currentSelfieName;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -30,8 +41,6 @@ public class MainActivity extends AppCompatActivity {
     myrecyclerview = (RecyclerView) findViewById(R.id.rv_selfielist);
 
     selfieList = new ArrayList<>();
-    selfieList.add(new Selfie("Selfie 1", "dummy_path"));
-    selfieList.add(new Selfie("Selfie 2", "dummy_path"));
 
     adapter = new SelfieListAdapter(this, selfieList);
     myrecyclerview.hasFixedSize();
@@ -56,11 +65,55 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null)
-            .show();
+        takePictureUsingIntent();
       }
     });
+  }
+
+  private void takePictureUsingIntent() {
+    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    // Ensure that there's a camera activity to handle the intent
+    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+      // Create the File where the photo should go
+      File photoFile = null;
+      try {
+        photoFile = createImageFile();
+      } catch (IOException ex) {
+        // Error occurred while creating the File
+      }
+
+      // Continue only if the File was successfully created
+      if (photoFile != null) {
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+      }
+    }
+  }
+
+  private File createImageFile() throws IOException {
+    // Create an image file name
+    currentSelfieName = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    File imageFile = File.createTempFile(currentSelfieName, ".jpg", getExternalFilesDir(null));
+    currentSelfiePath = imageFile.getAbsolutePath();
+    return imageFile;
+  }
+
+  @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+      File photoFile = new File(currentSelfiePath);
+      File selfieFile =
+          new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), currentSelfieName + ".jpg");
+      photoFile.renameTo(selfieFile);
+
+      Selfie selfie = new Selfie(currentSelfieName, Uri.fromFile(selfieFile).getPath());
+      selfieList.add(selfie);
+
+      adapter.notifyDataSetChanged();
+    } else {
+      File photoFile = new File(currentSelfiePath);
+      photoFile.delete();
+    }
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
